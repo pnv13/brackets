15e84249b5600d9809f87d7b87146f2146057a47
pnv13 <ivanpakhotnykh@mail.ru>
2021-02-07 15:52:00 +0300

    init: start and end brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+
+    if (str.length <= 1) false;
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        let brackets = bracketsConfig.find(
+            (b) => char === b[0] || char === b[1]
+        );
+        console.log(brackets);
+        if (char === brackets[0]) {
+            if (char === brackets[1] && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else if (
+            char === brackets[1] &&
+            stack[stack.length - 1] === brackets[0]
+        ) {
+            stack.pop();
+        } else if (
+            char === brackets[0] &&
+            stack[stack.length - 1] === brackets[0]
+        ) {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
